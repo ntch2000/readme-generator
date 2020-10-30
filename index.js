aea82d704d9870f0ef059a42d7ce3c79546c4c52
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-10-29 22:55:31 -0400

    Added initial readme questions and began creation of template readme outline

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
+const { title } = require("process");
+
+/* readme sections
+Project Title
+Description
+Table of Contents
+Installation
+Usage
+License
+Contributing
+Tests
+Questions
+*/
 
 // array of questions for user
-const questions = [];
+const questions = [
+  "What is your GitHub username?",
+  "What is your email address?",
+  "What is the title of your project?",
+  "What is the description of your project?",
+  "What are the instructions for installing this project?",
+  "What is the usage information for this project?",
+  "What licenses were used to create this project?",
+  "What are the contribution guidelines for this project",
+  "What test instructions should be included?",
+];
+
+// object of license options and badges
+
+const licenses = [];
 
 // function to write README file
 function writeToFile(fileName, data) {}
---
+++ readme-test.md
@@ -0,0 +1,24 @@
+# Project Title
+
+## Description
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+## Usage
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
