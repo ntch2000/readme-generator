39a242bc077888a288271a05aa6ab35977041b95
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-10-29 21:30:34 -0400

    Installed npm inquirer and set required variables in index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-];
+// array of questions for user
+const questions = [];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
