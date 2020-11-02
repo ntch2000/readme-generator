4ce1136293c2cc5f105de3f77a7f72a7cbe99eda
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-11-01 22:21:15 -0500

    Added additional license information and code to push license badge and text to response object

--- index.js
+++ index.js
@@ -54,16 +54,50 @@ const licenses = [
     name: "APACHE",
     value:
       "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    text: `Copyright [yyyy] [name of copyright owner]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+ 
+      http://www.apache.org/licenses/LICENSE-2.0
+ 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.`,
   },
   {
-    name: "GPL v3",
+    name: "GNU GPL v3",
     value:
       "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    text: `Copyright (C) <year>  <name of author>
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+  
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+  
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <https://www.gnu.org/licenses/>.`,
   },
   {
     name: "MIT",
     value:
       "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    text: `Copyright <YEAR> <COPYRIGHT HOLDER>
+
+      Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+      
+      The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`,
   },
 ];
 
@@ -109,7 +143,7 @@ function init() {
         name: "license",
         message: questions[6],
         type: "list",
-        choices: licenses,
+        choices: ["APACHE", "GPL v3", "MIT"],
       },
       {
         name: "contributions",
@@ -123,8 +157,20 @@ function init() {
       },
     ])
     .then((response) => {
-      console.log(response);
-      // console.log(response.license);
+      //console.log(response);
+      //console.log(response.license);
+      const licenseInfo = licenses.filter((license) => {
+        if (license.name === response.license) {
+          return license;
+        }
+      });
+
+      const { value, text } = licenseInfo[0];
+
+      //console.log(`${value} has this text: ${text}`);
+      response["licenseBadge"] = value;
+      response["licenseText"] = text;
+      //console.log(response);
       writeToFile("test.md", response);
     });
 }
@@ -132,4 +178,4 @@ function init() {
 // function call to initialize program
 init();
 
-console.log({ title });
+//console.log({ title });
---
+++ test.md
@@ -0,0 +1,40 @@
+# serser
+  
+  ## Description
+  
+  serqewr
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  erwqer
+  
+  ## Usage
+  
+  werwer  
+
+  ## License
+  
+  [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+  
+  ## Contributing
+  
+werwr
+
+  ## Tests
+  
+werwera
+
+  ## Questions
+  
+  If you have any questions regarding this project, please feel free to check out my GitHub [Test](https://github.com/Test).
+  
+  You can also reach me directly via my email address etset.
